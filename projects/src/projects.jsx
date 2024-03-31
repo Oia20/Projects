cc37e2531f61629733b9b8cc4949fc05204bdb55
Jacob <92279567+Oia20@users.noreply.github.com>
2024-03-30 21:22:00 -0500

    Changed styling

--- projects/src/projects.jsx
+++ projects/src/projects.jsx
@@ -5,7 +5,7 @@ const projects = [
   {
     id: 1,
     name: "OCR Automation Discord Bot",
-    desc: "Text extraction, data extry software, saving 10 precious human hours per week.",
+    desc: "Text extraction and automated data entry software, saving 10 precious human hours per week.",
     image: "eye.png",
     technologies: ["Python", "GCP", "PyTesseractOCR", "OpenCV"],
     description: "Members of an online gaming community I'm a part of were having to do 10+ hrs of manual data entry per week... Which is not what a game is meant for! Games are meant to bring joy, not to make you work. So I along with my partner made this bot which accepts a screenshot from the user, then using open computer vision and Googles TesseractOCR the bot extracts the text from the image. Next the bot automatically updates a spreadsheet using gSpread (A google spreadsheet API) giving each player in the screenshot the appropriate amount of game credits to be spent on in game gear. Lastly the bot returns a message describing which players were at the boss, and how many credits they received.",
@@ -19,8 +19,8 @@ const projects = [
     image: "portfolio.png",
     technologies: ["React Three Fiber", "Three.js", "JavaScript", "Blender", "React"],
     description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-    githubLink: null,
-    liveDemoLink: "https://example.com/project2-demo"
+    githubLink: "https://github.com/Oia20/personalPortfolio",
+    liveDemoLink: "https://oia20.github.io/personalPortfolio/"
   },
   {
     id: 3,
@@ -78,7 +78,9 @@ const ProjectList = () => {
 
   return (
     <div>
+      <div className='headerbg'>
       <h1 className="header">Jacob Dement's projects</h1>
+      </div>
       <div className="project-list">
         {projects.map((project) => (
           <ProjectCard
@@ -89,7 +91,18 @@ const ProjectList = () => {
           />
         ))}
       </div>
+      <footer>
+        <div class="footer-container">
+          <div class="footer-left">
+            <p>&copy; 2024 Jacob Dement. All rights reserved.</p>
+          </div>
+          <div class="footer-right">
+            <p><a href='https://oia20.github.io/simpleContact/'>Contact Page</a></p>
+          </div>
+        </div>
+      </footer>
     </div>
+    
   );
 };
 
