a726becb120098286b287943b2a1ee5ec8844fee
Jacob <92279567+Oia20@users.noreply.github.com>
2024-04-14 16:40:37 -0500

    Added Image, and changed title

--- projects/src/projects.jsx
+++ projects/src/projects.jsx
@@ -6,7 +6,7 @@ const projects = [
     id: 1,
     name: "MuseDrops, Fullstack Thought Sharing Platform",
     desc: "Website where you share your muses or 'Shower thoughts' with others.",
-    image: "eye.png",
+    image: "drops.png",
     technologies: ["JavaScript", "React", "Vite", "Node", "Express", "MySQL", "TailwindCSS", "Redux", "JSX", "CSS"],
     description: "I love the idea of shower thoughts and how they can be so silly, yet make you stop to think, so I made a website for people to share them! Unfortunately I can't keep the backend of the site live at the moment, so new posts and User Auth are currently unavailable. The Authentication encryption is done with the bcrpyt nodejs library to hash passwords. The database for this project uses MySQL, and the server is created using express/Node.js. The client side uses react, tailwindcss, redux for login state management, and vite for build tools.",
     githubLink: "https://github.com/Oia20/MuseDrops",
